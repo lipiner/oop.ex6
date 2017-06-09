6fad65520113138892fb45b0f682a27e8228290c
shahar <shahar.aizenbud@gmail.com>
2017-06-09 15:52:10 +0300

    added method1

--- untitled/src/balba.java
+++ untitled/src/balba.java
@@ -2,8 +2,8 @@
  * Created by shahar on 6/7/2017.
  */
 public class balba {
+    public int shaharIsTooYoung = 23;
 
     public void method1() {
-        public int shaharIsTooYoung = 23;
     }
 }
